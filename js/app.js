ad13e71352a4bb321565309eb560f6ac3cc76c44
rk <rebeccakruk@outlook.com>
2022-11-16 10:40:09 -0700

    added selection.html

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function pick(){
+    Cookies.set(`pick`);
+    // Cookies.set()
+}
+
+let favorite = Cookies.get(`pick`)
+// document.getElementById(`sony`).addEventListener(`click`, location.href="selection.html");
+// document.getElementById(`sonos`).addEventListener(`click`, location.href="selection.html");
+// document.getElementById(`bose`).addEventListener(`click`, location.href="selection.html");
